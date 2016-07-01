f3edaed73afc08cd668ae1e9d9038431c60a4aba
Lukas Liesis <liesislukas@gmail.com>
2016-07-01 18:30:39 +0300

    Update ranktrackr.js

--- ranktrackr.js
+++ ranktrackr.js
@@ -121,3 +121,115 @@ const ranktrackr_delete_group = ({group_id}) => {
       });
   });
 };
+
+const ranktrackr_get_urls = () => {
+  return new Promise((resolve, reject) => {
+    ranktrackr_get_access_token()
+      .then(access_token => {
+        ranktrackr_make_call(
+          {
+            method: 'get',
+            api_endpoint: '/urls',
+            form: {
+              access_token
+            }
+          }
+        )
+          .then(urls => {
+            let results = [];
+            Object.keys(urls).forEach(url => results.push({id: urls[url].id, url: urls[url].url}));
+            resolve(results);
+          })
+        ;
+      });
+  });
+};
+
+const ranktrackr_add_urls = ({urls, group_id, url_type}) => {
+  return new Promise((resolve, reject) => {
+    ranktrackr_get_access_token()
+      .then(access_token => {
+        ranktrackr_make_call(
+          {
+            method: 'post',
+            api_endpoint: '/urls/batch_create',
+            form: {
+              urls,
+              group_id,
+              url_type,
+              access_token
+            }
+          }
+        )
+          .then(response => {
+            resolve(response);
+          })
+        ;
+      });
+  });
+};
+
+const ranktrackr_delete_url = ({url_id}) => {
+  return new Promise((resolve, reject) => {
+    ranktrackr_get_access_token()
+      .then(access_token => {
+        ranktrackr_make_call(
+          {
+            method: 'delete',
+            api_endpoint: `/urls/${url_id}`,
+            form: {
+              access_token
+            }
+          }
+        )
+          .then(response => {
+            resolve(response);
+          })
+        ;
+      });
+  });
+};
+
+const ranktrackr_get_keywords = ({url_id}) => {
+  return new Promise((resolve, reject) => {
+    ranktrackr_get_access_token()
+      .then(access_token => {
+        ranktrackr_make_call(
+          {
+            method: 'get',
+            api_endpoint: `/urls/${url_id}/keywords`,
+            form: {
+              access_token
+            }
+          }
+        )
+          .then(keywords => {
+            let results = [];
+            Object.keys(keywords).forEach(keyword =>
+              results.push(
+                {
+                  id: keywords[keyword].id,
+                  keyword: keywords[keyword].query,
+                  uri: keywords[keyword].uri,
+                  url: keywords[keyword].result_url,
+                  mini_graph_uri: keywords[keyword].mini_graph,
+                }
+              )
+            );
+            resolve(results);
+          })
+        ;
+      });
+  });
+};
+
+
+export {
+  ranktrackr_get_groups,
+  ranktrackr_add_group,
+  ranktrackr_delete_group,
+  ranktrackr_get_urls,
+  ranktrackr_add_urls,
+  ranktrackr_delete_url,
+  ranktrackr_get_keywords,
+};
